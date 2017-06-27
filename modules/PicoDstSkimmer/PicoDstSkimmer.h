f4c173c7f6f0356f43f681a3dd7cd84fd87dc289
Daniel <jdb12@rice.edu>
2017-06-27 12:47:43 -0500

    Adding like sign

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -26,6 +26,10 @@ public:
 	struct TrackProxy {
 		StPicoTrack * _track = nullptr;
 		StPicoMtdPidTraits *_mtdPid = nullptr;
+		TLorentzVector _lv;
+		StThreeVectorD _p;
+		StPhysicalHelixD _h;
+
 	};
 
 
@@ -54,6 +58,49 @@ protected:
 	vector< shared_ptr<TrackProxy> > pip;
 	vector< shared_ptr<TrackProxy> > pim;
 
+
+	TLorentzVector analyzePair( StPicoEvent * _event, shared_ptr<TrackProxy> pos, shared_ptr<TrackProxy> neg ){
+		float bField = _event->bField() * kilogauss;
+
+		TLorentzVector lv;
+		lv.SetPtEtaPhiM( 0, 0, 0, 0 );
+
+		pair<double,double> pathLengths = pos->_h.pathLengths(neg->_h);
+
+		StThreeVectorD pNegPosAtDca = pos->_h.at(pathLengths.first);
+		StThreeVectorD pPosPosAtDca = neg->_h.at(pathLengths.second);
+
+		StThreeVectorD dcaVector = pNegPosAtDca - pPosPosAtDca;
+		StThreeVectorD primVtxPos = _event->primaryVertex();
+		StThreeVectorD secVtxPos = pPosPosAtDca + ( dcaVector * 0.5 );
+		StThreeVectorD decLenVec = secVtxPos - primVtxPos;
+
+		if (decLenVec.mag() < 2.7) return lv; // cut on path length
+		if (dcaVector.mag() > 1.5) return lv; // cut on track mutual dca
+		
+		StThreeVectorD pNegMomAtDca = pos->_h.momentumAt(pathLengths.first, bField );	// CHECK THIS
+		StThreeVectorD pPosMomAtDca = neg->_h.momentumAt(pathLengths.second, bField );
+
+		StThreeVectorD K0sMomAtDCA = pNegMomAtDca + pPosMomAtDca;
+
+		pos->_lv.SetPtEtaPhiM( pNegMomAtDca.perp(), pNegMomAtDca.pseudoRapidity(), pNegMomAtDca.phi(), MASS_PI );
+		neg->_lv.SetPtEtaPhiM( pPosMomAtDca.perp(), pPosMomAtDca.pseudoRapidity(), pPosMomAtDca.phi(), MASS_PI );
+
+		lv = pos->_lv + neg->_lv;
+
+		double K0pT = lv.Pt();
+		double pointingAngle = K0sMomAtDCA.angle(decLenVec);
+		if (TMath::Abs(pointingAngle) > 0.106+0.056-0.1123*K0pT+0.025*K0pT*K0pT) return lv;
+
+
+		// book->fill( "pointingAngle_pt", K0pT, pointingAngle );
+		double openingAngle = pNegMomAtDca.angle(pPosMomAtDca);
+		// book->fill( "openingAngle", openingAngle );
+		// book->fill( prefix + "_pt_mass", lv.M(), K0pT );
+
+		return lv;
+
+	}
 
 	void analyzePairs( StPicoEvent * _event ){
 
@@ -131,6 +178,18 @@ protected:
 			} // loop on pim
 		} // loop on pip
 
+		// ##################### Like-sign #########################
+		for ( size_t i = 0; i < pip.size()-1; i++ ){
+			auto p1 = pip[i];
+			for ( size_t j = i+1; j < pip.size(); j++ ){
+				auto p2 = pip[j];
+
+				TLorentzVector lv = analyzePair( _event, p1, p2 );
+				book->fill( "ls_tpc_pt_mass", lv.Pt(), lv.M() );
+			}
+		}
+		// ##################### Like-sign #########################
+
 	}
 
 
@@ -140,6 +199,8 @@ protected:
 		if ( nullptr == event ){
 			return;
 		}
+
+		double bField = event->bField() * kilogauss;
 
 		// if ( event->refMult() > 100 ) return;
 
@@ -164,10 +225,10 @@ protected:
 			// if ( !track->isHFTTrack() ) continue; 
 			if ( abs(nHitsFit ) < 10 ) continue;
 			if ( abs( nHitsRatio ) < 0.52 ) continue;
-			float gP = track->helix().momentum( event->bField() * kilogauss ).magnitude();
+			float gP = track->helix().momentum( bField ).magnitude();
 			// book->fill( "gMom", gP );
 			if ( gP <= 0 ) continue;
-			float gDCA = track->globalDCA( event->bField() * kilogauss, pVtx );
+			float gDCA = track->globalDCA( bField, pVtx );
 			// book->fill( "gDCA", gDCA );
 			if ( gDCA < 1.3 || gDCA > 9 ) continue;
 			if ( abs( track->nSigmaPion() ) > 3.0 ) continue;
@@ -175,6 +236,11 @@ protected:
 			auto tp = shared_ptr<TrackProxy>( new TrackProxy() );
 			tp->_track = track;
 			tp->_mtdPid = mtdPid;
+
+			tp->_h = tp->_track->helix();
+			tp->_p = tp->_h.momentum( bField );
+			tp->_lv.SetPtEtaPhiM( tp->_p.perp(), tp->_p.pseudoRapidity(), tp->_p.phi(), MASS_PI );
+		
 
 			if ( track->charge() > 0 )
 				pip.push_back( tp );
